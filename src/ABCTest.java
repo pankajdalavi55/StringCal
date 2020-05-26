7d6b7508852c1c7326ec5cfbe3b127e5eb0e9c46
Captain <pdalavi55@gmail.com>
2020-05-26 16:38:19 +0530

    Initial Project

---
+++ src/ABCTest.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import cal.Calculator;
+
+class ABCTest {
+
+	@Test
+	void test() {
+		Calculator obj = new Calculator();
+		fail("Not yet implemented");
+	}
+
+}
